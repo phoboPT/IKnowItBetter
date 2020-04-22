362ac1172ce942d194a06d7ebad9b522708b2859
phoboPT <hugo.branco.c@gmail.com>
2020-04-22 15:30:35 +0100

    teste

---
+++ helper.h
@@ -0,0 +1,6 @@
+#ifndef HELPER_H_INCLUDED
+#define HELPER_H_INCLUDED
+
+
+
+#endif // HELPER_H_INCLUDED
---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+
+
+int main(){
+setlocale(LC_ALL,"Portuguese");
+
+
+
+
+
+return 0;
+}
