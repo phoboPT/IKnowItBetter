75caca3216304fc38acfe40d7506386dffa51f87
phoboPT <hugo.branco.c@gmail.com>
2020-04-22 15:37:36 +0100

    teste

--- data.h
+++ data.h
@@ -1,6 +1,6 @@
 #ifndef HELPER_H_INCLUDED
 #define HELPER_H_INCLUDED
-
+//Estruturas
 
 
 #endif // HELPER_H_INCLUDED
--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <locale.h>
 #include <string.h>
+#include "data.h"
 
 
 int main(){
